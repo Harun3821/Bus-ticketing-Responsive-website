60ccc00a7b0e0d47b9c19b96abf6d20503ae74db
Harun3821 <harun4382183@gmail.com>
2024-02-19 01:46:20 +0600

    first commit PopUp Section

--- Scprit.js
+++ Scprit.js
@@ -4,3 +4,13 @@ function chakani(){
     document.getElementById('item-list').classList.toggle('hidden');
 })
 }
+ 
+
+// -----PopUp----------//
+
+function openPoupp(){
+document.getElementById('Card-popup').style.display = 'block';
+}
+function closePopup(){
+document.getElementById('Card-popup').style.display = 'none';
+}
