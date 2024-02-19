13fb4d233fad657922cc44a53702bf78e425c8cc
Harun3821 <harun4382183@gmail.com>
2024-02-19 16:04:51 +0600

    first commit footer section

--- Scprit.js
+++ Scprit.js
@@ -14,3 +14,18 @@ document.getElementById('Card-popup').style.display = 'block';
 function closePopup(){
 document.getElementById('Card-popup').style.display = 'none';
 }
+
+
+// --------coler section----------//
+
+ const buttons = document.querySelectorAll('.btn');
+
+ // Add click event listener to each button
+ buttons.forEach(button => {
+   button.addEventListener('click', () => {
+     // Change background color on click
+     button.style.backgroundColor = '#22ff45'; // Change this to whatever color you want
+   });
+ });
+
+
